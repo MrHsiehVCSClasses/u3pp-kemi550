e68bab2a102396bc67089325e9c7e5b9c7555c7f
Kenneth Min <kennethmin@Kenneths-MacBook-Pro-5.local>
2022-10-24 10:54:23 -0700

    u3pp Part A

---
+++ src/main/java/u3pp/Blackjack.java
@@ -0,0 +1,28 @@
+package u3pp;
+
+import java.util.Scanner;
+
+public class Blackjack {
+    
+    public Blackjack() {
+
+    }
+    public void play(Scanner scanner) {
+        
+    }
+    public static int calcPoints(Card[] hand) {
+        return 0;
+    }
+    public static String determineResult(Card[] userHand, Card[] dealerHand) {
+        return null;
+    }
+    public static boolean isBust(Card[] hand) {
+        return false;
+    }
+    public static boolean isBlackjack(Card[] hand) {
+        return false;
+    }
+    public static boolean shouldDealerKeepHitting(Card[] hand) {
+        return false;
+    }
+}
--- src/main/java/u3pp/PalindromeTester.java
+++ src/main/java/u3pp/PalindromeTester.java
@@ -8,7 +8,25 @@ public class PalindromeTester {
      * @return true if the string is a palindrome
      */
     public static boolean isPalindrome(String s) {
-
-        return true;
+        
+        s = s.replace(" ", "");
+        
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--){
+            rev += s.charAt(i);
+        }
+        
+        boolean palindrome = true;
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) != rev.charAt(i)){
+                palindrome = false;
+            }
+        }
+        if (palindrome) {
+            return true;
+        } else {
+            return false;
+        }
+        
     }
 }
